9da7562f90ce506de15d28f6047250f926a451cd
BioStaCs-public <biostacs_github@163.com>
2025-09-03 15:01:16 +0800

    Refactor: Code structure corrections and README improvements

--- NeoantigenPipline/run.sh
+++ NeoantigenPipline/run.sh
@@ -1,4 +1,4 @@
 export TF_CPP_MIN_LOG_LEVEL=2. 
 export CUDA_VISIBLE_DEVICES="".
 export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64
-python Neoantigen.py -c ../../../configures/Neoantigen_configure.yaml -p ../../../configures/Neoantigen_pathes.yaml
+python Neoantigen.py -c ../configures/Neoantigen_configure.yaml -p ../configures/Neoantigen_pathes.yaml
